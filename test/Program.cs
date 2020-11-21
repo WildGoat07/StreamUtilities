53e9f59ae39059fb5947ed734f325db5e1183e94
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-21 16:47:33 +0100

    created projects

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StreamUtilities;
+
+namespace test
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            var stream = new TemporaryStream(5);
+            stream.Dispose();
+            stream.Dispose();
+        }
+
+        #endregion Private Methods
+    }
+}
